2e94942c931b87afd56cf5cfa321b8e4b8159637
Bram Buijs <bdbuijs@gmail.com>
2023-07-18 14:55:44 +0200

    added SSH support for 1Password

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -34,6 +34,12 @@
         nvim-treesitter.withAllGrammars
       ];
     };
+    ssh = {
+      enable = true;
+      extraConfig = ''
+              Host *
+        	    IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+    };
     starship.enable = true;
     starship.enableZshIntegration = true;
     zsh = {
