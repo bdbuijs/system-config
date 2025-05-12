91649da2156f8ee0b113c4b406169838b2e9afcb
Bram Buijs <bram.buijs@hurricanecommerce.com>
2025-05-13 00:09:20 +0200

    add vimrc file, attempt 1

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   fonts.fontconfig.enable = true;
   home.stateVersion = "23.05";
   home.packages = with pkgs; [
@@ -29,22 +29,20 @@
     kitty = {
       enable = true;
       settings = {
+        font = "JetBrainsMono Nerd Font";
         confirm_os_window_close = 0;
         dynamic_background_opacity = true;
         enable_audio_bell = false;
         mouse_hide_wait = "-1.0";
-        window_padding_width = 10;
-        background_opacity = "0.75";
+        window_padding_width = 5;
+        background_opacity = "0.85";
         background_blur = 5;
       };
     };
     neovim =
       {
         enable = true;
-        extraConfig = ''
-          set number relativenumber
-          set scrolloff=8
-        '';
+        extraConfig = lib.fileContents ./vimrc;
         plugins = with pkgs.vimPlugins; [
           nvim-treesitter.withAllGrammars
         ];
